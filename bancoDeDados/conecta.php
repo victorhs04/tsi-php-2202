716ee8eaf40698c970d07ffd92c7435a29edad63
VICTOR HUGO SOUZA OLIVEIRA <victor.hsoliveira15@senacsp.edu.br>
2022-09-21 22:16:27 -0300

    aula/21-09-2022

---
+++ bancoDeDados/conecta.php
@@ -0,0 +1,11 @@
+<?php
+
+//Dados para nos conectarmos ao SGBD
+$dsn = 'mysql:dbname=senac;host=localhost;port=3307';
+
+$user = 'root';
+$pass = '';
+
+$bd = new PDO($dsn, $user, $pass);
+
+var_dump($bd);
